1aa64f7c73d4da72d8cb857d56b8c52d0ebd53d4
Terraform05 <alexjoaramirez@gmail.com>
2023-12-16 22:46:11 -0600

    clean file

---
+++ cleanup.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+from sensitive import OPENAI_API_KEY, OPENAI_ORGANIZATION
+
+client = OpenAI(
+    organization=OPENAI_ORGANIZATION,
+    api_key=OPENAI_API_KEY
+)
+
+for assistant in client.beta.assistants.list():
+    client.beta.assistants.delete(assistant.id)
+
+print(client.beta.assistants.list())
